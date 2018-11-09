43d87de7f8d0f75ab4b6f6adbcf75b1430638870
s <lentilspring@gmail.com>
2018-11-10 03:22:20 +0900

    *subisoテストプログラム一段落

--- test_sdl.cpp
+++ test_sdl.cpp
@@ -27,132 +27,183 @@ subiso::space
 g_space;
 
 
-subiso::renderer  g_front_renderer;
-subiso::renderer  g_back_renderer;
-subiso::renderer  g_left_renderer;
-subiso::renderer  g_right_renderer;
+window
+g_window;
 
 
-subiso::renderer*  g_current_renderer=&g_front_renderer;
-
-
-subiso::plane*
-g_last_plane;
-
-
-void
-process_input() noexcept
+class
+shell: public widget
 {
-  static bool  lock;
+  subiso::renderer  m_front;
+  subiso::renderer  m_back;
+  subiso::renderer  m_left;
+  subiso::renderer  m_right;
 
-    for(auto&  pt: g_point_buffer)
-    {
-      static int  last_x;
-      static int  last_y;
+  subiso::renderer*  m_current=nullptr;
 
-      int  x = pt.x/subiso::g_plane_size;
-      int  y = pt.y/subiso::g_plane_size;
+  point  m_last_point;
 
+  subiso::plane*  m_last_plane=nullptr;
 
-      auto&  view = g_current_renderer->get_view();
+  bool  m_lock=false;
 
-      auto&  ref = g_current_renderer->get_refstack(x,y).get_current();
+  void  process(subiso::plane_reference_stack::element&  ref) noexcept
+  {
+    auto  nd = ref.get_node();
 
-        if((x != last_x) ||
-           (y != last_y))
-        {
-          lock = false;
+      if(nd)
+      {
+        auto&  box = *nd->m_box;
 
-          last_x = x;
-          last_y = y;
-        }
+          if(g_input.test_mouse_right())
+          {
+            box.m_kind = subiso::box::kind::null;
 
+            m_lock = true;
+          }
 
-        if(!g_input.test_mouse_left() &&
-           !g_input.test_mouse_right())
-        {
-          lock = false;
-        }
-
-
-      g_last_plane = ref.get_plane();
-
-        if(!lock)
-        {
-          auto  nd = ref.get_node();
-
-            if(nd)
-            {
-              auto&  box = *nd->m_box;
-
-                if(g_input.test_mouse_right())
-                {
-                  box.m_kind = subiso::box::kind::null;
-
-                  lock = true;
-                }
-
-              else
-                if(g_input.test_mouse_left())
-                {
-                    if(g_last_plane->is_top() && nd->m_up_node)
-                    {
-                      nd->m_up_node->m_box->m_kind = subiso::box::kind::earth;
-                    }
+        else
+          if(g_input.test_mouse_left())
+          {
+              if(m_last_plane->is_top() && nd->m_up_node)
+              {
+                nd->m_up_node->m_box->m_kind = subiso::box::kind::earth;
+              }
 
 
-                  lock = true;
-                }
-            }
+            m_lock = true;
+          }
+      }
 
-          else
-            if(g_input.test_mouse_left())
-            {
-              nd = ((&ref)-1)->get_node();
+    else
+      if(g_input.test_mouse_left())
+      {
+        nd = ((&ref)-1)->get_node();
 
-                if(nd)
-                {
-                  nd->m_box->m_kind = subiso::box::kind::earth;
+          if(nd)
+          {
+            nd->m_box->m_kind = subiso::box::kind::earth;
 
-                  lock = true;
-                }
-            }
-        }
-    }
-}
+            m_lock = true;
+          }
+      }
+  }
+
+public:
+  shell(subiso::space&  sp) noexcept{assign(sp);}
+
+  subiso::renderer*  get_current() const noexcept{return m_current;}
+
+  void  revolve_to_left() noexcept
+  {
+    m_current = (m_current == &m_front)? &m_left
+               :(m_current == &m_left)?  &m_back
+               :(m_current == &m_back)?  &m_right
+               :&m_front;
+
+    request_redraw();
+  }
+
+  void  revolve_to_right() noexcept
+  {
+    m_current = (m_current == &m_front)? &m_right
+               :(m_current == &m_left)?  &m_front
+               :(m_current == &m_back)?  &m_left
+               :&m_back;
+
+    request_redraw();
+  }
+
+  shell&  assign(subiso::space&  sp) noexcept
+  {
+    m_front.assign(subiso::space_view(sp));
+    m_left.assign(m_front.get_view().make_revolved());
+    m_back.assign( m_left.get_view().make_revolved());
+    m_right.assign(m_back.get_view().make_revolved());
+
+    m_current = &m_front;
+
+    request_reform();
+
+    return *this;
+  }
+
+  void  process_before_reform() noexcept override
+  {
+    int  w = std::max(m_front.get_image_width() ,m_left.get_image_width() );
+    int  h = std::max(m_front.get_image_height(),m_left.get_image_height());
+
+    set_content_width( w);
+    set_content_height(h);
+  }
+
+  void  do_on_mouse_act(point  mouse_pos) noexcept
+  {
+    int  x = mouse_pos.x/subiso::g_plane_size;
+    int  y = mouse_pos.y/subiso::g_plane_size;
+
+    auto&  view = m_current->get_view();
+
+    auto&  ref = m_current->get_refstack(x,y).get_current();
+
+      if((x != m_last_point.x) ||
+         (y != m_last_point.y))
+      {
+        m_lock = false;
+
+        m_last_point.x = x;
+        m_last_point.y = y;
+      }
 
 
-void
-update_screen() noexcept
-{
-  g_screen_canvas.fill(color());
-
-  g_current_renderer->render(g_screen_canvas);
-
-    if(g_last_plane)
-    {
-      string_form  sf;
-
-      auto  i = g_last_plane->m_box->m_index;
-
-//      auto  flags = g_last_box->m_top_plane.get_flags();
-
-      static character_color  chcolor(colors::black,colors::white);
-
-      g_screen_canvas.draw_string(chcolor,sf("X:%3d",i.x),0, 0);
-      g_screen_canvas.draw_string(chcolor,sf("Y:%3d",i.y),0,16);
-      g_screen_canvas.draw_string(chcolor,sf("Z:%3d",i.z),0,32);
-/*
-      g_screen_canvas.draw_string(chcolor,sf("%s%s%s%s",flags&top_flag? "T":"-",
-                                                        flags&bottom_flag? "B":"-",
-                                                        flags&left_flag? "L":"-",
-                                                        flags&right_flag? "R":"-"),0,48);
-*/
-    }
+      if(!g_input.test_mouse_left() &&
+         !g_input.test_mouse_right())
+      {
+        m_lock = false;
+      }
 
 
-  sdl::update_screen(g_screen_canvas);
-}
+    m_last_plane = ref.get_plane();
+
+      if(!m_lock)
+      {
+        process(ref);
+
+        request_redraw();
+      }
+  }
+
+  void  render(const canvas&  cv) noexcept override
+  {
+    cv.fill(color());
+
+    m_current->render(cv);
+
+      if(m_last_plane)
+      {
+        string_form  sf;
+
+        auto  i = m_last_plane->m_box->m_index;
+
+  //      auto  flags = g_last_box->m_top_plane.get_flags();
+
+        static character_color  chcolor(colors::black,colors::white);
+
+        cv.draw_string(chcolor,sf("X:%3d",i.x),0, 0);
+        cv.draw_string(chcolor,sf("Y:%3d",i.y),0,16);
+        cv.draw_string(chcolor,sf("Z:%3d",i.z),0,32);
+  /*
+        g_screen_canvas.draw_string(chcolor,sf("%s%s%s%s",flags&top_flag? "T":"-",
+                                                          flags&bottom_flag? "B":"-",
+                                                          flags&left_flag? "L":"-",
+                                                          flags&right_flag? "R":"-"),0,48);
+  */
+      }
+  }
+
+};
+
+
 
 
 void
@@ -160,30 +211,24 @@ main_loop() noexcept
 {
   sdl::update_control();
 
-    if(g_modified_input.test_p() && g_input.test_p())
-    {
-//      g_space.update_box_all();
+  g_window.do_total_reform_if_necessary();
 
-      g_current_renderer = (g_current_renderer == &g_front_renderer)? &g_left_renderer
-                          :(g_current_renderer == &g_left_renderer)?  &g_back_renderer
-                          :(g_current_renderer == &g_back_renderer)?  &g_right_renderer
-                          :&g_front_renderer;
-
-      update_screen();
-    }
-
-  else
     if(g_point_buffer.size())
     {
-      process_input();
-
-      update_screen();
+        for(auto&  pt: g_point_buffer)
+        {
+          g_window.process_user_input(pt);
+        }
     }
 
-  else
-    if(g_needed_to_redraw)
+
+    if(g_window.redraw_if_necessary() || g_needed_to_redraw)
     {
-      update_screen();
+      g_screen_canvas.fill(color());
+
+      g_screen_canvas.draw_canvas(g_window.get_content_image(),0,0);
+
+      sdl::update_screen(g_screen_canvas);
     }
 }
 
@@ -237,16 +282,20 @@ main(int  argc, char**  argv)
     }}
 
 
-  g_front_renderer.assign(subiso::space_view(g_space));
-  g_left_renderer.assign(g_front_renderer.get_view().make_revolved());
-  g_back_renderer.assign(g_left_renderer.get_view().make_revolved());
-  g_right_renderer.assign(g_back_renderer.get_view().make_revolved());
+  auto  l_button = new button(new iconshow({&icons::left }),[](button_event  evt){if(evt.is_press()){evt->get_userdata<shell>()->revolve_to_left() ;}});
+  auto  r_button = new button(new iconshow({&icons::right}),[](button_event  evt){if(evt.is_press()){evt->get_userdata<shell>()->revolve_to_right();}});
 
-  sdl::init(g_front_renderer.get_image_width(),g_front_renderer.get_image_height());
+  auto  buttons = make_row({l_button,r_button});
+
+  auto  sh = new shell(g_space);
+
+  set_userdata({l_button,r_button},sh);
+
+  g_window.set_root_widget(make_column({buttons,sh}));
+
+  sdl::init(g_window.get_width(),g_window.get_height());
 
   g_screen_canvas = sdl::make_screen_canvas();
-
-  update_screen();
 
 #ifdef __EMSCRIPTEN__
   emscripten_set_main_loop(main_loop,0,false);
